90ab9735fdd5a4906509a9cfabde604be3b623a6
Pratik <pratik682019@gmail.com>
2023-03-26 21:52:42 +0530

    2nd commit

--- src/app/mainfooter/mainfooter.component.ts
+++ src/app/mainfooter/mainfooter.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-mainfooter',
   templateUrl: './mainfooter.component.html',
-  styleUrls: ['./mainfooter.component.scss']
+  styleUrls: ['./mainfooter.component.scss'],
 })
 export class MainfooterComponent {
-
+  // @ViewChild('scroll')
+  // scroll!: ElementRef;
+  // scrollTop() {
+  //   this.scroll.nativeElement.scrollTop = 0;
+  // }
 }
